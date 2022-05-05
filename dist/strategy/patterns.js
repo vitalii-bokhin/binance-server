888f0716bcec8b3b95ceaa9791a0281685453868
Lufter <lufter21@gmail.com>
2022-05-05 08:14:24 +0300

    com

--- dist/strategy/patterns.js
+++ dist/strategy/patterns.js
@@ -13,19 +13,15 @@ const splitCdl = function (cdl) {
         body = cdl.open - cdl.close;
         lowTail = cdl.close - cdl.low;
     }
-    return {
-        highTail,
-        body,
-        lowTail
-    };
+    return { highTail, body, lowTail };
 };
 function Patterns({ symbol, candlesData, tiSettings, levelsOpt, trendsOpt }) {
     const _candles = candlesData;
-    const fourthCandle = _candles.slice(-4)[0];
-    const thirdCandle = _candles.slice(-3)[0];
-    const prevCandle = _candles.slice(-2)[0];
-    const lastCandle = _candles.slice(-1)[0];
-    const lastPrice = lastCandle.close;
+    const cdl_3 = _candles.slice(-4)[0];
+    const cdl_2 = _candles.slice(-3)[0];
+    const cdl_1 = _candles.slice(-2)[0];
+    const cdl_0 = _candles.slice(-1)[0];
+    const lastPrice = cdl_0.close;
     const symbolResult = {
         symbol,
         position: null,
@@ -50,102 +46,137 @@ function Patterns({ symbol, candlesData, tiSettings, levelsOpt, trendsOpt }) {
         symbolResult.preferIndex = 100 - percentLoss;
         symbolResult.resolvePosition = true;
     };
-    const thirdCdlSplit = splitCdl(thirdCandle);
-    const prevCdlSplit = splitCdl(prevCandle);
-    if ( // hummer
-    fourthCandle.close < fourthCandle.open
-        && thirdCdlSplit.highTail < thirdCdlSplit.body
-        && thirdCdlSplit.lowTail > thirdCdlSplit.body * 2
-        && thirdCandle.high < fourthCandle.high
-        && thirdCandle.high > fourthCandle.low
-        && prevCandle.close > prevCandle.open
-        && lastPrice > prevCandle.high) {
-        long(prevCandle.low);
-        console.log(symbol, 'hummer');
-    }
-    else if ( // shooting star
-    fourthCandle.close > fourthCandle.open
-        && thirdCdlSplit.highTail > thirdCdlSplit.body * 2
-        && thirdCdlSplit.lowTail < thirdCdlSplit.body
-        && thirdCandle.low > fourthCandle.low
-        && thirdCandle.low < fourthCandle.high
-        && prevCandle.close < prevCandle.open
-        && lastPrice < prevCandle.low) {
-        short(prevCandle.high);
-        console.log(symbol, 'shooting star');
-    }
-    else if ( // hanging man
-    thirdCdlSplit.highTail < thirdCdlSplit.body
-        && thirdCdlSplit.lowTail > thirdCdlSplit.body * 2
-        && thirdCandle.high > fourthCandle.high
-        && prevCandle.close < prevCandle.open
-        && lastPrice < prevCandle.low) {
-        short(prevCandle.high);
-        console.log(symbol, 'hanging man');
-    }
-    else if ( // inverted hummer
-    thirdCdlSplit.highTail > thirdCdlSplit.body * 2
-        && thirdCdlSplit.lowTail < thirdCdlSplit.body
-        && thirdCandle.low < fourthCandle.low
-        && prevCandle.close > prevCandle.open
-        && lastPrice > prevCandle.high) {
-        long(prevCandle.low);
-        console.log(symbol, 'inverted hummer');
-    }
-    else if ( // spinning top long
-    thirdCdlSplit.highTail > thirdCdlSplit.body * 2
-        && thirdCdlSplit.lowTail > thirdCdlSplit.body * 2
-        && fourthCandle.close < fourthCandle.open
-        && prevCandle.close > prevCandle.open
-        && prevCdlSplit.highTail * 2 < prevCdlSplit.body
-        && lastPrice > prevCandle.high) {
-        long(prevCandle.low);
-        console.log(symbol, 'spinning top long');
-    }
-    else if ( // spinning top short
-    thirdCdlSplit.highTail > thirdCdlSplit.body * 2
-        && thirdCdlSplit.lowTail > thirdCdlSplit.body * 2
-        && fourthCandle.close > fourthCandle.open
-        && prevCandle.close < prevCandle.open
-        && prevCdlSplit.lowTail * 2 < prevCdlSplit.body
-        && lastPrice < prevCandle.low) {
-        short(prevCandle.high);
-        console.log(symbol, 'spinning top short');
-    }
-    else if ( // bullish engulfing
-    fourthCandle.close < fourthCandle.open
-        && thirdCandle.close < thirdCandle.open
-        && prevCandle.close > prevCandle.open
-        && prevCandle.close > thirdCandle.high
-        && lastPrice > prevCandle.high) {
-        long(prevCandle.low);
+    // const cdl_2_Split = splitCdl(cdl_2);
+    const cdl_1_Split = splitCdl(cdl_1);
+    if ( // bullish engulfing
+    cdl_2.close < cdl_2.open
+        && cdl_1.close > cdl_1.open
+        && cdl_1.high >= cdl_2.high
+        && cdl_1.low <= cdl_2.low
+        && cdl_1.close >= cdl_2.open
+        && cdl_0.close > cdl_0.open) {
+        long(cdl_1.low);
         console.log(symbol, 'bullish engulfing');
     }
     else if ( // bearish engulfing
-    fourthCandle.close > fourthCandle.open
-        && thirdCandle.close > thirdCandle.open
-        && prevCandle.close < prevCandle.open
-        && prevCandle.close < thirdCandle.low
-        && lastPrice < prevCandle.low) {
-        short(prevCandle.high);
+    cdl_2.close > cdl_2.open
+        && cdl_1.close < cdl_1.open
+        && cdl_1.high >= cdl_2.high
+        && cdl_1.low <= cdl_2.low
+        && cdl_1.close <= cdl_2.open
+        && cdl_0.close < cdl_0.open) {
+        short(cdl_1.high);
         console.log(symbol, 'bearish engulfing');
     }
-    else if ( // tweezer top
-    prevCandle.close > prevCandle.open
-        && lastCandle.close < lastCandle.open
-        && lastCandle.high - lastCandle.open == prevCandle.high - prevCandle.close
-        && lastPrice < prevCandle.open) {
-        short(prevCandle.high > lastCandle.high ? prevCandle.high : lastCandle.high);
-        console.log(symbol, 'tweezer top');
+    else if ( // pinbar long
+    cdl_1_Split.lowTail > cdl_1_Split.body * 2
+        && cdl_1_Split.highTail <= cdl_1_Split.body
+        && lastPrice > cdl_1.high) {
+        long(cdl_1.low);
+        console.log(symbol, 'pinbar long');
     }
-    else if ( // tweezer bottom
-    prevCandle.close < prevCandle.open
-        && lastCandle.close > lastCandle.open
-        && lastCandle.open - lastCandle.low == prevCandle.close - prevCandle.low
-        && lastPrice > prevCandle.open) {
-        long(prevCandle.low < lastCandle.low ? prevCandle.low : lastCandle.low);
-        console.log(symbol, 'tweezer bottom');
+    else if ( // pinbar short
+    cdl_1_Split.highTail > cdl_1_Split.body * 2
+        && cdl_1_Split.lowTail <= cdl_1_Split.body
+        && lastPrice < cdl_1.low) {
+        short(cdl_1.high);
+        console.log(symbol, 'pinbar short');
     }
+    // if ( // hummer
+    //     fourthCandle.close < fourthCandle.open
+    //     && thirdCdlSplit.highTail < thirdCdlSplit.body
+    //     && thirdCdlSplit.lowTail > thirdCdlSplit.body * 2
+    //     && thirdCandle.high < fourthCandle.high
+    //     && thirdCandle.high > fourthCandle.low
+    //     && prevCandle.close > prevCandle.open
+    //     && lastPrice > prevCandle.high
+    // ) {
+    //     long(prevCandle.low);
+    //     console.log(symbol, 'hummer');
+    // } else if ( // shooting star
+    //     fourthCandle.close > fourthCandle.open
+    //     && thirdCdlSplit.highTail > thirdCdlSplit.body * 2
+    //     && thirdCdlSplit.lowTail < thirdCdlSplit.body
+    //     && thirdCandle.low > fourthCandle.low
+    //     && thirdCandle.low < fourthCandle.high
+    //     && prevCandle.close < prevCandle.open
+    //     && lastPrice < prevCandle.low
+    // ) {
+    //     short(prevCandle.high);
+    //     console.log(symbol, 'shooting star');
+    // } else if ( // hanging man
+    //     thirdCdlSplit.highTail < thirdCdlSplit.body
+    //     && thirdCdlSplit.lowTail > thirdCdlSplit.body * 2
+    //     && thirdCandle.high > fourthCandle.high
+    //     && prevCandle.close < prevCandle.open
+    //     && lastPrice < prevCandle.low
+    // ) {
+    //     short(prevCandle.high);
+    //     console.log(symbol, 'hanging man');
+    // } else if ( // inverted hummer
+    //     thirdCdlSplit.highTail > thirdCdlSplit.body * 2
+    //     && thirdCdlSplit.lowTail < thirdCdlSplit.body
+    //     && thirdCandle.low < fourthCandle.low
+    //     && prevCandle.close > prevCandle.open
+    //     && lastPrice > prevCandle.high
+    // ) {
+    //     long(prevCandle.low);
+    //     console.log(symbol, 'inverted hummer');
+    // } else if ( // spinning top long
+    //     thirdCdlSplit.highTail > thirdCdlSplit.body * 2
+    //     && thirdCdlSplit.lowTail > thirdCdlSplit.body * 2
+    //     && fourthCandle.close < fourthCandle.open
+    //     && prevCandle.close > prevCandle.open
+    //     && prevCdlSplit.highTail * 2 < prevCdlSplit.body
+    //     && lastPrice > prevCandle.high
+    // ) {
+    //     long(prevCandle.low);
+    //     console.log(symbol, 'spinning top long');
+    // } else if ( // spinning top short
+    //     thirdCdlSplit.highTail > thirdCdlSplit.body * 2
+    //     && thirdCdlSplit.lowTail > thirdCdlSplit.body * 2
+    //     && fourthCandle.close > fourthCandle.open
+    //     && prevCandle.close < prevCandle.open
+    //     && prevCdlSplit.lowTail * 2 < prevCdlSplit.body
+    //     && lastPrice < prevCandle.low
+    // ) {
+    //     short(prevCandle.high);
+    //     console.log(symbol, 'spinning top short');
+    // } else if ( // bullish engulfing
+    //     fourthCandle.close < fourthCandle.open
+    //     && thirdCandle.close < thirdCandle.open
+    //     && prevCandle.close > prevCandle.open
+    //     && prevCandle.close > thirdCandle.high
+    //     && lastPrice > prevCandle.high
+    // ) {
+    //     long(prevCandle.low);
+    //     console.log(symbol, 'bullish engulfing');
+    // } else if ( // bearish engulfing
+    //     fourthCandle.close > fourthCandle.open
+    //     && thirdCandle.close > thirdCandle.open
+    //     && prevCandle.close < prevCandle.open
+    //     && prevCandle.close < thirdCandle.low
+    //     && lastPrice < prevCandle.low
+    // ) {
+    //     short(prevCandle.high);
+    //     console.log(symbol, 'bearish engulfing');
+    // } else if ( // tweezer top
+    //     prevCandle.close > prevCandle.open
+    //     && lastCandle.close < lastCandle.open
+    //     && lastCandle.high - lastCandle.open == prevCandle.high - prevCandle.close
+    //     && lastPrice < prevCandle.open
+    // ) {
+    //     short(prevCandle.high > lastCandle.high ? prevCandle.high : lastCandle.high);
+    //     console.log(symbol, 'tweezer top');
+    // } else if ( // tweezer bottom
+    //     prevCandle.close < prevCandle.open
+    //     && lastCandle.close > lastCandle.open
+    //     && lastCandle.open - lastCandle.low == prevCandle.close - prevCandle.low
+    //     && lastPrice > prevCandle.open
+    // ) {
+    //     long(prevCandle.low < lastCandle.low ? prevCandle.low : lastCandle.low);
+    //     console.log(symbol, 'tweezer bottom');
+    // }
     if (symbolResult.resolvePosition) {
         console.log(symbolResult);
     }
--- src/strategy/patterns.ts
+++ src/strategy/patterns.ts
@@ -16,22 +16,18 @@ const splitCdl = function (cdl: Candle): { highTail: number; body: number; lowTa
         lowTail = cdl.close - cdl.low;
     }
 
-    return {
-        highTail,
-        body,
-        lowTail
-    };
+    return { highTail, body, lowTail };
 }
 
 export function Patterns({ symbol, candlesData, tiSettings, levelsOpt, trendsOpt }: Entry): SymbolResult {
     const _candles = candlesData;
 
-    const fourthCandle: Candle = _candles.slice(-4)[0];
-    const thirdCandle: Candle = _candles.slice(-3)[0];
-    const prevCandle: Candle = _candles.slice(-2)[0];
-    const lastCandle: Candle = _candles.slice(-1)[0];
+    const cdl_3: Candle = _candles.slice(-4)[0];
+    const cdl_2: Candle = _candles.slice(-3)[0];
+    const cdl_1: Candle = _candles.slice(-2)[0];
+    const cdl_0: Candle = _candles.slice(-1)[0];
 
-    const lastPrice = lastCandle.close;
+    const lastPrice = cdl_0.close;
 
     const symbolResult: SymbolResult = {
         symbol,
@@ -62,114 +58,153 @@ export function Patterns({ symbol, candlesData, tiSettings, levelsOpt, trendsOpt
         symbolResult.resolvePosition = true;
     }
 
-    const thirdCdlSplit = splitCdl(thirdCandle);
-    const prevCdlSplit = splitCdl(prevCandle);
+    // const cdl_2_Split = splitCdl(cdl_2);
+    const cdl_1_Split = splitCdl(cdl_1);
 
-    if ( // hummer
-        fourthCandle.close < fourthCandle.open
-        && thirdCdlSplit.highTail < thirdCdlSplit.body
-        && thirdCdlSplit.lowTail > thirdCdlSplit.body * 2
-        && thirdCandle.high < fourthCandle.high
-        && thirdCandle.high > fourthCandle.low
-        && prevCandle.close > prevCandle.open
-        && lastPrice > prevCandle.high
+    if ( // bullish engulfing
+        cdl_2.close < cdl_2.open
+        && cdl_1.close > cdl_1.open
+        && cdl_1.high >= cdl_2.high
+        && cdl_1.low <= cdl_2.low
+        && cdl_1.close >= cdl_2.open
+        && cdl_0.close > cdl_0.open
     ) {
-        long(prevCandle.low);
-        console.log(symbol, 'hummer');
-
-    } else if ( // shooting star
-        fourthCandle.close > fourthCandle.open
-        && thirdCdlSplit.highTail > thirdCdlSplit.body * 2
-        && thirdCdlSplit.lowTail < thirdCdlSplit.body
-        && thirdCandle.low > fourthCandle.low
-        && thirdCandle.low < fourthCandle.high
-        && prevCandle.close < prevCandle.open
-        && lastPrice < prevCandle.low
-    ) {
-        short(prevCandle.high);
-        console.log(symbol, 'shooting star');
-
-    } else if ( // hanging man
-        thirdCdlSplit.highTail < thirdCdlSplit.body
-        && thirdCdlSplit.lowTail > thirdCdlSplit.body * 2
-        && thirdCandle.high > fourthCandle.high
-        && prevCandle.close < prevCandle.open
-        && lastPrice < prevCandle.low
-    ) {
-        short(prevCandle.high);
-        console.log(symbol, 'hanging man');
-
-    } else if ( // inverted hummer
-        thirdCdlSplit.highTail > thirdCdlSplit.body * 2
-        && thirdCdlSplit.lowTail < thirdCdlSplit.body
-        && thirdCandle.low < fourthCandle.low
-        && prevCandle.close > prevCandle.open
-        && lastPrice > prevCandle.high
-    ) {
-        long(prevCandle.low);
-        console.log(symbol, 'inverted hummer');
-
-    } else if ( // spinning top long
-        thirdCdlSplit.highTail > thirdCdlSplit.body * 2
-        && thirdCdlSplit.lowTail > thirdCdlSplit.body * 2
-        && fourthCandle.close < fourthCandle.open
-        && prevCandle.close > prevCandle.open
-        && prevCdlSplit.highTail * 2 < prevCdlSplit.body
-        && lastPrice > prevCandle.high
-    ) {
-        long(prevCandle.low);
-        console.log(symbol, 'spinning top long');
-
-    } else if ( // spinning top short
-        thirdCdlSplit.highTail > thirdCdlSplit.body * 2
-        && thirdCdlSplit.lowTail > thirdCdlSplit.body * 2
-        && fourthCandle.close > fourthCandle.open
-        && prevCandle.close < prevCandle.open
-        && prevCdlSplit.lowTail * 2 < prevCdlSplit.body
-        && lastPrice < prevCandle.low
-    ) {
-        short(prevCandle.high);
-        console.log(symbol, 'spinning top short');
-
-    } else if ( // bullish engulfing
-        fourthCandle.close < fourthCandle.open
-        && thirdCandle.close < thirdCandle.open
-        && prevCandle.close > prevCandle.open
-        && prevCandle.close > thirdCandle.high
-        && lastPrice > prevCandle.high
-    ) {
-        long(prevCandle.low);
+        long(cdl_1.low);
         console.log(symbol, 'bullish engulfing');
 
     } else if ( // bearish engulfing
-        fourthCandle.close > fourthCandle.open
-        && thirdCandle.close > thirdCandle.open
-        && prevCandle.close < prevCandle.open
-        && prevCandle.close < thirdCandle.low
-        && lastPrice < prevCandle.low
+        cdl_2.close > cdl_2.open
+        && cdl_1.close < cdl_1.open
+        && cdl_1.high >= cdl_2.high
+        && cdl_1.low <= cdl_2.low
+        && cdl_1.close <= cdl_2.open
+        && cdl_0.close < cdl_0.open
     ) {
-        short(prevCandle.high);
+        short(cdl_1.high);
         console.log(symbol, 'bearish engulfing');
 
-    } else if ( // tweezer top
-        prevCandle.close > prevCandle.open
-        && lastCandle.close < lastCandle.open
-        && lastCandle.high - lastCandle.open == prevCandle.high - prevCandle.close
-        && lastPrice < prevCandle.open
+    } else if ( // pinbar long
+        cdl_1_Split.lowTail > cdl_1_Split.body * 2
+        && cdl_1_Split.highTail <= cdl_1_Split.body
+        && lastPrice > cdl_1.high
     ) {
-        short(prevCandle.high > lastCandle.high ? prevCandle.high : lastCandle.high);
-        console.log(symbol, 'tweezer top');
+        long(cdl_1.low);
+        console.log(symbol, 'pinbar long');
 
-    } else if ( // tweezer bottom
-        prevCandle.close < prevCandle.open
-        && lastCandle.close > lastCandle.open
-        && lastCandle.open - lastCandle.low == prevCandle.close - prevCandle.low
-        && lastPrice > prevCandle.open
+    } else if ( // pinbar short
+        cdl_1_Split.highTail > cdl_1_Split.body * 2
+        && cdl_1_Split.lowTail <= cdl_1_Split.body
+        && lastPrice < cdl_1.low
     ) {
-        long(prevCandle.low < lastCandle.low ? prevCandle.low : lastCandle.low);
-        console.log(symbol, 'tweezer bottom');
-
+        short(cdl_1.high);
+        console.log(symbol, 'pinbar short');
     }
+
+    // if ( // hummer
+    //     fourthCandle.close < fourthCandle.open
+    //     && thirdCdlSplit.highTail < thirdCdlSplit.body
+    //     && thirdCdlSplit.lowTail > thirdCdlSplit.body * 2
+    //     && thirdCandle.high < fourthCandle.high
+    //     && thirdCandle.high > fourthCandle.low
+    //     && prevCandle.close > prevCandle.open
+    //     && lastPrice > prevCandle.high
+    // ) {
+    //     long(prevCandle.low);
+    //     console.log(symbol, 'hummer');
+
+    // } else if ( // shooting star
+    //     fourthCandle.close > fourthCandle.open
+    //     && thirdCdlSplit.highTail > thirdCdlSplit.body * 2
+    //     && thirdCdlSplit.lowTail < thirdCdlSplit.body
+    //     && thirdCandle.low > fourthCandle.low
+    //     && thirdCandle.low < fourthCandle.high
+    //     && prevCandle.close < prevCandle.open
+    //     && lastPrice < prevCandle.low
+    // ) {
+    //     short(prevCandle.high);
+    //     console.log(symbol, 'shooting star');
+
+    // } else if ( // hanging man
+    //     thirdCdlSplit.highTail < thirdCdlSplit.body
+    //     && thirdCdlSplit.lowTail > thirdCdlSplit.body * 2
+    //     && thirdCandle.high > fourthCandle.high
+    //     && prevCandle.close < prevCandle.open
+    //     && lastPrice < prevCandle.low
+    // ) {
+    //     short(prevCandle.high);
+    //     console.log(symbol, 'hanging man');
+
+    // } else if ( // inverted hummer
+    //     thirdCdlSplit.highTail > thirdCdlSplit.body * 2
+    //     && thirdCdlSplit.lowTail < thirdCdlSplit.body
+    //     && thirdCandle.low < fourthCandle.low
+    //     && prevCandle.close > prevCandle.open
+    //     && lastPrice > prevCandle.high
+    // ) {
+    //     long(prevCandle.low);
+    //     console.log(symbol, 'inverted hummer');
+
+    // } else if ( // spinning top long
+    //     thirdCdlSplit.highTail > thirdCdlSplit.body * 2
+    //     && thirdCdlSplit.lowTail > thirdCdlSplit.body * 2
+    //     && fourthCandle.close < fourthCandle.open
+    //     && prevCandle.close > prevCandle.open
+    //     && prevCdlSplit.highTail * 2 < prevCdlSplit.body
+    //     && lastPrice > prevCandle.high
+    // ) {
+    //     long(prevCandle.low);
+    //     console.log(symbol, 'spinning top long');
+
+    // } else if ( // spinning top short
+    //     thirdCdlSplit.highTail > thirdCdlSplit.body * 2
+    //     && thirdCdlSplit.lowTail > thirdCdlSplit.body * 2
+    //     && fourthCandle.close > fourthCandle.open
+    //     && prevCandle.close < prevCandle.open
+    //     && prevCdlSplit.lowTail * 2 < prevCdlSplit.body
+    //     && lastPrice < prevCandle.low
+    // ) {
+    //     short(prevCandle.high);
+    //     console.log(symbol, 'spinning top short');
+
+    // } else if ( // bullish engulfing
+    //     fourthCandle.close < fourthCandle.open
+    //     && thirdCandle.close < thirdCandle.open
+    //     && prevCandle.close > prevCandle.open
+    //     && prevCandle.close > thirdCandle.high
+    //     && lastPrice > prevCandle.high
+    // ) {
+    //     long(prevCandle.low);
+    //     console.log(symbol, 'bullish engulfing');
+
+    // } else if ( // bearish engulfing
+    //     fourthCandle.close > fourthCandle.open
+    //     && thirdCandle.close > thirdCandle.open
+    //     && prevCandle.close < prevCandle.open
+    //     && prevCandle.close < thirdCandle.low
+    //     && lastPrice < prevCandle.low
+    // ) {
+    //     short(prevCandle.high);
+    //     console.log(symbol, 'bearish engulfing');
+
+    // } else if ( // tweezer top
+    //     prevCandle.close > prevCandle.open
+    //     && lastCandle.close < lastCandle.open
+    //     && lastCandle.high - lastCandle.open == prevCandle.high - prevCandle.close
+    //     && lastPrice < prevCandle.open
+    // ) {
+    //     short(prevCandle.high > lastCandle.high ? prevCandle.high : lastCandle.high);
+    //     console.log(symbol, 'tweezer top');
+
+    // } else if ( // tweezer bottom
+    //     prevCandle.close < prevCandle.open
+    //     && lastCandle.close > lastCandle.open
+    //     && lastCandle.open - lastCandle.low == prevCandle.close - prevCandle.low
+    //     && lastPrice > prevCandle.open
+    // ) {
+    //     long(prevCandle.low < lastCandle.low ? prevCandle.low : lastCandle.low);
+    //     console.log(symbol, 'tweezer bottom');
+
+    // }
 
 
 
